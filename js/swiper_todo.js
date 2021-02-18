5e198b3f8cc7b671f9222a1920965486e44adb5b
Dima Kranin <dimashooter.dk@gmail.com>
2021-02-18 10:28:02 +0200

    FB-ads specialist webpage

---
+++ js/swiper_todo.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    
+  },
+});
